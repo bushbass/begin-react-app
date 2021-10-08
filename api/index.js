bb8f0ac83a4f488ab6d371bfaa55009d10abac50
Alex Nielsen <alex.nielsen@gmail.com>
2021-10-08 10:16:33 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 exports.handler = async function http (req) {
   console.log('Begin API called')
+const data= await  fetch('https://v6.exchangerate-api.com/v6/1f88a2cead881e5036e60f6b/latest/USD')
   return {
     headers: {
       'content-type': 'application/json; charset=utf8',
@@ -7,7 +8,7 @@ exports.handler = async function http (req) {
     },
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Hello from your Begin API!'
+      message: 'Hello from your Begin API!',data
     })
   }
 }
